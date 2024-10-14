1331aa73aabc84a37791332bc19aac6608c9e80f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-10-14 11:12:39 +0000

    Working on the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,10 @@ export default function Sidebar() {
   return (
     <div>
         <h2>The Brutal Martian Landscape</h2>
+        <div>
+            <h3>Description</h3>
+            <h3>Description</h3>
+        </div>
     </div>
   )
 }
