d195793db0f1b6980e13b13b4e7bb9b26c3e7901
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-10-14 11:33:44 +0000

    Styling the main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,11 @@
 export default function Main() {
   return (
-    <div>
-        <img src="../../public/mars.png" alt="mars-demo-picture" />
+    <div className="imgContainer">
+      <img
+        src="../../public/mars.png"
+        alt="mars-demo-picture"
+        className="bgImage"
+      />
     </div>
-  )
+  );
 }
