f505558fcae1d9b8892d8e0a31ad16d74ca4b5b8
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-10-14 12:04:38 +0000

    Working on the background gradient for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
 export default function Footer() {
   return (
     <footer>
-        <div className="bgGradient"></div>
+        {/* <div className="bgGradient"></div> */}
         <div>
             <h2>The Brutal Martian Lanscape</h2>
             <h1>The APOD Project</h1>
         </div>
+        <button>
         <i className="fa-solid fa-circle-info"></i>
+        </button>
     </footer>
   )
 }
