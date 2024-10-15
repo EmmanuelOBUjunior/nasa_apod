989fbb3ca9cf745fbfc8a2af075dc5ed8feb9f07
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-10-15 08:49:25 +0000

    Added the right arrow to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,20 @@
 export default function Sidebar() {
   return (
     <div className="sidebar">
-        <div className="bgOverlay"></div>
-        <div className="sidebarContents">
+      <div className="bgOverlay"></div>
+      <div className="sidebarContents">
         <h2>The Brutal Martian Landscape</h2>
         <div>
-            <h3>Description</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui illo incidunt saepe molestiae fuga nobis perferendis eius neque, animi, beatae voluptates numquam veniam, obcaecati ipsum nam sint quos aliquam accusantium!</p>
+          <h3>Description</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui illo
+            incidunt saepe molestiae fuga nobis perferendis eius neque, animi,
+            beatae voluptates numquam veniam, obcaecati ipsum nam sint quos
+            aliquam accusantium!
+          </p>
         </div>
-        </div>
+        <i className="fa-solid fa-arrow-right"></i>
+      </div>
     </div>
-  )
+  );
 }
