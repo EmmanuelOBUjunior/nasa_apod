033d0a19a8d0ed3d524afc649268f75b17cd3594
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-10-15 09:17:39 +0000

    Working on the logic to show modal conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react"
 import Footer from "./components/Footer"
 import Main from "./components/Main"
 import Sidebar from "./components/Sidebar"
 
 function App() {
+    const [showModal, setshowModal] = useState(false)
  
   return (
     <>
