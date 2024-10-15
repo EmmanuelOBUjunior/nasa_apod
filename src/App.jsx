9aa6189b2d4482ed82e471c9111228720b371744
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-10-15 10:08:12 +0000

    Fetching data from nasa api

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
-import { useState } from "react"
-import Footer from "./components/Footer"
-import Main from "./components/Main"
-import Sidebar from "./components/Sidebar"
+import { useEffect, useState } from "react";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-    const [showModal, setshowModal] = useState(false)
+  const [showModal, setshowModal] = useState(false);
 
-    function handleToggleModal(){
-      setshowModal(!showModal)
+  function handleToggleModal() {
+    setshowModal(!showModal);
+  }
+
+  useEffect(() => {
+    const fetchNASAData = async()=>{
+      const NASA_API = import.meta.env.NASA_API_KEY
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_API}`
+
+      try{
+        const res = await fetch(url)
+        const data = await res.json()
+        console.log("DATA\n",data)
+      }catch(err){
+        console.log(err.message)
+      }
+
     }
- 
+    fetchNASAData()
+  }, []);
+
   return (
     <>
-      <Main/>
-      {showModal && <Sidebar handleToggleModal = {handleToggleModal}/>}
-      <Footer handleToggleModal = {handleToggleModal}/>
+      <Main />
+      {showModal && <Sidebar handleToggleModal={handleToggleModal} />}
+      <Footer handleToggleModal={handleToggleModal} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
