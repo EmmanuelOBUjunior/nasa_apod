17df93159347b29fa7a9e733395339fdd6183dcb
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-10-15 08:43:47 +0000

    Styling the sidebar contents

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,12 +3,11 @@ export default function Sidebar() {
     <div className="sidebar">
         <div className="bgOverlay"></div>
         <div className="sidebarContents">
-          
-        </div>
         <h2>The Brutal Martian Landscape</h2>
         <div>
             <h3>Description</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui illo incidunt saepe molestiae fuga nobis perferendis eius neque, animi, beatae voluptates numquam veniam, obcaecati ipsum nam sint quos aliquam accusantium!</p>
+        </div>
         </div>
     </div>
   )
