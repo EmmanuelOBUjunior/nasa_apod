7b83171dd8fd050e84b2788c60b1379593c8a2e4
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-10-18 20:03:24 +0000

    fetching data from NASA Api

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,14 +4,11 @@ export default function Sidebar({handleToggleModal, data}) {
     <div className="sidebar">
       <div className="bgOverlay" onClick={handleToggleModal}></div>
       <div className="sidebarContents">
-        <h2>The Brutal Martian Landscape</h2>
+        <h2>{data.title}</h2>
         <div>
           <h3>Description</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui illo
-            incidunt saepe molestiae fuga nobis perferendis eius neque, animi,
-            beatae voluptates numquam veniam, obcaecati ipsum nam sint quos
-            aliquam accusantium!
+            {data.explanation}
           </p>
         </div>
         <button onClick={handleToggleModal}><i className="fa-solid fa-arrow-right"></i></button>
@@ -21,5 +18,6 @@ export default function Sidebar({handleToggleModal, data}) {
 }
 
 Sidebar.propTypes = {
-  handleToggleModal: PropTypes.func.isRequired
+  handleToggleModal: PropTypes.func.isRequired,
+  data: PropTypes.object
 }
